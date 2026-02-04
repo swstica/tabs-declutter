8578e59e922ab17e464d3053efe8097d4e28c5ba
Swastika Yadav <Swastikayadav5358@gmail.com>
2026-02-04 16:16:54 +0530

    feat: add web app core files (api, styles, lib, root)

---
+++ tabs-declutter-test/web/api.ts
@@ -0,0 +1,5 @@
+// Sets up the API client for interacting with your backend.
+// For your API reference, visit: https://docs.gadget.dev/api/tabs-declutter-test
+import { Client } from "@gadget-client/tabs-declutter-test";
+
+export const api = new Client();
